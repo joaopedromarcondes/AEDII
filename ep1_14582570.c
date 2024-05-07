3566b565b2cf87f1ef3befd8977c45ad54bfd89c
Joao Pedro <joaopopm@usp.br>
2024-05-07 16:40:46 -0300

    Agora os vertices de articulacao funcionam

--- ep1_14582570.c
+++ ep1_14582570.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef enum {
     BRANCO,
@@ -104,39 +105,78 @@ void mostrar_busca(int* antecessores, int nv, bool eh_BP, int* ordem) {
     }
 }
 
-void visitaBP(Grafo* g, int v, cor* cores, int* antecessores, int* componentes_conectados, int contador, int* tempo_de_encontro, int* ordem) {
+void mostrar_vertices_de_articulacao(bool* vertices_de_articulacao, int nv) {
+    printf("Vertices de articulacao:\n");
+    int i;
+    for (i = 0; i < nv; i++) {
+        if (vertices_de_articulacao[i]) printf("%d ", i);
+    }
+    printf("\n");
+}
+
+int min(int a, int b) {
+    if (a < b) return a;
+    return b;
+}
+
+void visitaBP(Grafo* g, int v, cor* cores, int* antecessores, int* componentes_conectados, int contador, int* tempo_de_encontro, int* ordem, bool* vertices_de_articulacao, int* low, int* tempo_de_descoberta) {
     cores[v] = CINZA;
     Apontador atual = primeiroListaAdj(v, g);
     int u;
     ordem[*tempo_de_encontro] = v;
+    tempo_de_descoberta[v] = *tempo_de_encontro;
+    low[v] = *tempo_de_encontro;
+    //printf("vertice: %d low: %d\n", v, low[v]);
     (*tempo_de_encontro)++;
+    int contador_de_filhos = 0;
     while (atual != VERTICE_INVALIDO) {
         u = obtemVerticeDestino(atual, g);
         if (u < 0 || u >= obtemNrVertices(g)) break;
         if (cores[u] == BRANCO) {
             antecessores[u] = v;
-            visitaBP(g, u, cores, antecessores, componentes_conectados, contador, tempo_de_encontro, ordem);
+            visitaBP(g, u, cores, antecessores, componentes_conectados, contador, tempo_de_encontro, ordem, vertices_de_articulacao, low, tempo_de_descoberta);
+            contador_de_filhos++;
+            low[v] = min(low[u], low[v]);
+
+            if ( antecessores[v] != -1 && low[u] >= tempo_de_descoberta[v]) {
+                vertices_de_articulacao[v] = true;
+            }
+        } else if (cores[u] == CINZA) {
+            if (antecessores[v] != u) {
+                low[v] = tempo_de_descoberta[u];
+            }
         }
         atual = proxListaAdj(v, g, atual);
     }
     componentes_conectados[v] = contador;
     cores[v] = PRETO;
+
+    if (antecessores[v] == -1 && contador_de_filhos > 1) {
+        vertices_de_articulacao[v] = true;
+    }
+    
+    //printf("vertice: %d tempo: %d low: %d\n", v, tempo_de_descoberta[v], low[v]);
 }
 
-void buscaProfundidade(Grafo* g, int* componentes_conectados, int* antecessores, int* ordem) {
+void buscaProfundidade(Grafo* g, int* componentes_conectados, int* antecessores, int* ordem, bool* vertices_de_articulacao) {
     int nv = obtemNrVertices(g);
     cor* cores = (cor*) malloc(sizeof(cor)*nv);
+    int* low = (int*) malloc(sizeof(int)*nv);
+    int* tempo_de_descoberta = (int*) malloc(sizeof(int)*nv);
     int i;
     for (i = 0; i < nv; i++) {
         cores[i] = BRANCO;
         antecessores[i] = -1;
+        vertices_de_articulacao[i] = false;
+        tempo_de_descoberta[i] = -1;
+        low[i] = -1;
     }
     int j, contador = 0;
     int* tempo_de_encontro = (int*) malloc(sizeof(int));
     *tempo_de_encontro = 0;
     for (j = 0; j < nv; j++) {
         if (cores[j] == BRANCO) {
-            visitaBP(g, j, cores, antecessores, componentes_conectados, contador, tempo_de_encontro, ordem);
+            visitaBP(g, j, cores, antecessores, componentes_conectados, contador, tempo_de_encontro, ordem, vertices_de_articulacao, low, tempo_de_descoberta);
             contador++;
         }
     } 
@@ -170,10 +210,9 @@ void visitaBL(Grafo* g, int v, cor* cores, int* antecessores) {
 }
 
 
-void buscaLargura(Grafo* g) {
+void buscaLargura(Grafo* g, int* antecessores) {
     int nv = obtemNrVertices(g);
     cor* cores = (cor*) malloc(sizeof(cor)*nv);
-    int* antecessores = (int*) malloc(sizeof(int)*nv);
     int i;
     for (i = 0; i < nv; i++) {
         cores[i] = BRANCO;
@@ -200,7 +239,7 @@ int main() {
 
     FILE *fp;
 
-    fp = fopen("entrada2.txt", "r");
+    fp = fopen("entrada1.txt", "r");
     int nv, na; // nv - numero de vertices, na - numero de arestasg
     fscanf(fp, "%d %d\n", &nv, &na);
 
@@ -227,12 +266,16 @@ int main() {
     int* componentes_conectados = (int*) malloc(sizeof(int)*nv);
     int* antecessores_bp = (int*) malloc(sizeof(int)*nv);
     int* ordem_bp = (int*) malloc(sizeof(int)*nv);
+    bool* vertices_de_articulacao = (bool*) malloc(sizeof(bool)*nv);
+
+    int* antecessores_bl = (int*) malloc(sizeof(int)*nv);
     
-    buscaLargura(&g);
-    buscaProfundidade(&g, componentes_conectados, antecessores_bp, ordem_bp);
+    buscaLargura(&g, antecessores_bl);
+    buscaProfundidade(&g, componentes_conectados, antecessores_bp, ordem_bp, vertices_de_articulacao);
     
     mostrar_busca(antecessores_bp, nv, true, ordem_bp);
     mostrar_componentes_conectados(componentes_conectados, nv);
+    mostrar_vertices_de_articulacao(vertices_de_articulacao, nv);
     
     return 0;
 }
